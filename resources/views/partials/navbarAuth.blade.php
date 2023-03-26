9bff871974c6803fb5aa2be2f890d52f25d9c5f4
Apakahboleh <rofidragon71@gmail.com>
2023-03-26 22:10:06 +0700

    menambahkan function AOS sebagai durasi animasi #4 resolve

--- resources/views/partials/navbarAuth.blade.php
+++ resources/views/partials/navbarAuth.blade.php
@@ -91,7 +91,7 @@
 <script src="/js/aos.js"></script>
 <script>
     AOS.init({
-        duration: 800,
+        duration: 800, // menambahkan function AOS sebagai durasi animasi
     });
 </script>
 <script>

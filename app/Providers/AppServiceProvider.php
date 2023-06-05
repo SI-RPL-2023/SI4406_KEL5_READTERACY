c36a1e5bec4c974953a6eb149eef036e3938a50f
Apakahboleh <rofidragon71@gmail.com>
2023-06-05 12:20:45 +0700

    membuat pagination pada catalogue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 use App\Models\User;
+use App\Models\Genre;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Paginator::useBootstrap();
         Gate::define("admin", function ( User $user ) {
             return $user->role;
         });
@@ -31,6 +35,11 @@ class AppServiceProvider extends ServiceProvider
 
         Gate::define("pegawai", function ( User $user ) {
             return $user->role;
+        });
+
+        View::composer('layouts.navbar', function ($view) {
+            $genre = Genre::all();
+            $view->with('genre', $genre);
         });
     }
 }
